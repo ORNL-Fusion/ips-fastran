706dd5fbecb4f600725c0b58f5cd19119f8a0799
JM <parkjm@fusion.gat.com>
2020-06-19 15:20:32 -0700

    higher level of ips component for massive serial

--- src/ips_massive_serial_global.py
+++ src/ips_massive_serial_global.py
@@ -11,7 +11,7 @@ import time
 from configobj import ConfigObj
 from component import Component
 
-class ips_massive_serial(Component):
+class ips_massive_serial_global(Component):
 
     def __init__(self, services, config):
         Component.__init__(self, services, config)
@@ -32,8 +32,6 @@ class ips_massive_serial(Component):
         services.stage_input_files(self.INPUT_FILES)
 
         #--- setup
-        wrt_localconf("local.conf")
-
         dir_summary = getattr(self, "SUMMARY", "SUMMARY")
         dir_summary = os.path.realpath(dir_summary)
         if not os.path.exists(dir_summary): os.makedirs(dir_summary)
@@ -41,59 +39,43 @@ class ips_massive_serial(Component):
         f_sim_config = getattr(self, "SIMULATION", "")
         sim = ConfigObj(f_sim_config, interpolation='template', file_error=True)
 
-        f_inscan = getattr(self, "INSCAN", "inscan")
-        with open(f_inscan,"r") as f: inscan = f.readlines()
-        nsim = len(inscan)-1
-        header = inscan[0]
+        f_machine_config = getattr(self, "MACHINE", "")
 
-        dask_nodes = int(getattr(self, "DASK_NODES", "1"))
-        task_ppn = int(getattr(self, "TASK_PPN", ""))
+        ntasks = int(getattr(self, "NTASKS", "1"))
+        dask_nodes = 1 
+        task_ppn = 1
 
-        rank = int(getattr(self, "RANK", "0"))
-        size = int(getattr(self, "SIZE", "1"))
-        print(('NSIM = %d at RANK = %d'%(nsim, rank)))
-
-        try: 
-            pwd = services.get_config_param("PWD")
-        except:
-            pwd = os.environ["PWD"]
+        print('NTASKS = %d'%ntasks)
 
         #--- add to pool
         pool = services.create_task_pool('pool')
         cwd = services.get_working_dir()
         tasks = {}
-        for k in range(rank, nsim, size): 
+        for k in range(ntasks): 
             rundir = os.path.realpath("run%05d"%k)
             logfile = "ipslog.%05d"%k
             if not os.path.exists(rundir): os.makedirs(rundir)
 
-            data = inscan[k+1]
-            for i, key in enumerate(header.split()):
-                print (i, key)
-                comp, vname, vtype = key.split(':') 
-                d = data.split() 
-                if vtype == 'str': val = str(d[i])
-                else: val = eval(vtype+"("+d[i]+")")
-                if comp == '': sim[vname] = val
-                else: sim[comp][vname] = val 
-
-            sim["PWD"] = pwd #os.environ["PWD"]
+            sim["PWD"] = os.environ["PWD"]
             sim["RUN_ID"] = "run%05d"%k
             sim["SIM_ROOT"] = rundir
             sim["OUT_REDIRECT"] = "True"
             sim["OUT_REDIRECT_FNAME"] = os.path.join(cwd, "run%05d.out"%k)
             sim["USE_PORTAL"] = "False"
+
             driver = sim['PORTS']['DRIVER']['IMPLEMENTATION'] 
-            sim[driver]["SUMMARY"] = dir_summary
+            sim[driver]["SUMMARY"] = dir_summary#+"%05d"%k
+            sim[driver]["RANK"] = k
+            sim[driver]["SIZE"] = ntasks 
             sim.write(open("run%05d.config"%k, "wb"))
 
             ips_bin = os.path.join(self.BIN_PATH, self.BIN) 
             ips_bin += " --config=run%05d.config"%k
             ips_bin += " --log=ips_%05d.log"%k
-            ips_bin += " --platform=local.conf"
+            ips_bin += " --platform=%s"%f_machine_config
             if self.clean_after: 
                 ips_bin += "\nrm -rf "+rundir
-                ips_bin += "\nrm -f "+rundir+".zip"
+                ips_bin += "\nrm -f "+rundir+f_machine_config
 
             with open(os.path.join(rundir, "ips_bin.sh"), "w") as f: f.write(ips_bin)
 
@@ -106,7 +88,8 @@ class ips_massive_serial(Component):
                 os.path.join(rundir, "ips_bin.sh"),
                 logfile=logfile,
                 timeout=self.time_out,
-                task_ppn=task_ppn)
+                task_ppn=task_ppn
+            )
 
         #--- run
         ret_val = services.submit_tasks('pool', use_dask=True, dask_nodes=dask_nodes)
@@ -122,21 +105,3 @@ class ips_massive_serial(Component):
 
     def finalize(self, timeStamp=0):
         pass  
-
-def wrt_localconf(fname="local.conf"):
-    s = \
-"""HOST = local
-MPIRUN = eval
-NODE_DETECTION = manual
-TOTAL_PROCS = 1
-NODES = 1
-PROCS_PER_NODE = 1
-CORES_PER_NODE = 1
-SOCKETS_PER_NODE = 1
-NODE_ALLOCATION_MODE = SHARED
-USE_ACCURATE_NODES = ON
-"""
-    f=open(fname, "w")
-    f.write(s)
-    f.close()
-
