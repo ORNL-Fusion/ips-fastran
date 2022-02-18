43eff8d9cf87e344ed52b5661a5db1131c5b2fe9
JM <parkjm@fusion.gat.com>
2022-02-18 11:45:35 -0800

    more general pedestal component

--- src/fastran/stability/pedestal.py
+++ src/fastran/stability/pedestal.py
@@ -7,128 +7,49 @@
 from ipsframework import Component
 from fastran.plasmastate.plasmastate import plasmastate
 
-from numpy import *
+import numpy as np
 from Namelist import Namelist
 from fastran.util.modelprofile import profile_pedestal
 from fastran.util.zinterp import zinterp
 from fastran.util.formula import get_ni
+from fastran.util.loglinear import loglinear
+from fastran.stability import pedestal_io
 
-class model_pedestal(Component):
+class pedestal(Component):
     def __init__(self, services, config):
         Component.__init__(self, services, config)
         print('Created %s' % (self.__class__))
 
-    def init(self, timeStamp=0):
+    def init(self, timeid=0):
         print('pedestal.init() called')
 
-    def step(self, timeStamp=0):
+    def step(self, timeid=0):
         print('pedestal.step() started')
 
-        #--- entry
-        services = self.services
-
         #--- stage plasma state files
-        services.stage_state()
+        self.services.stage_state()
 
         #--- get plasma state file name
-        cur_state_file = services.get_config_param('CURRENT_STATE')
-        cur_bc_file = services.get_config_param('CURRENT_BC')
-        cur_instate_file = services.get_config_param('CURRENT_INSTATE')
-        cur_eqdsk_file = services.get_config_param('CURRENT_EQDSK')
+        cur_state_file = self.services.get_config_param('CURRENT_STATE')
+        cur_instate_file = self.services.get_config_param('CURRENT_INSTATE')
+        cur_eqdsk_file = self.services.get_config_param('CURRENT_EQDSK')
 
         #--- stage input files
-        services.stage_input_files(self.INPUT_FILES)
+        self.services.stage_input_files(self.INPUT_FILES)
 
-        #--- apply constraint to local plasma state
-        inbc = Namelist(cur_bc_file)
-        r0  = inbc["inbc"]["r0"][0]
-        b0  = inbc["inbc"]["b0"][0]
-        ip  = inbc["inbc"]['ip'][0]
+        #--- eped parameters 
+        ps_backend = getattr(self, 'PS_BACKEND', 'PS')
+        fn_inped = getattr(self, 'INPED', 'inped')
 
-        instate = Namelist(cur_instate_file)
-        neped = instate["instate"]["ne_ped"][0]
-        nesep = instate["instate"]["ne_sep"][0]/neped
-
-        inped = self.INPED
-        wped_fit = loglinear(inped,'wped')
-        teped_fit = loglinear(inped,'teped')
-
-        wped = wped_fit.get(ip=ip, neped=neped, nesep=nesep)
-        pped = pped_fit.get(ip=ip, neped=neped, nesep=nesep)
-
-        zeffped = instate["instate"]["zeff_axis"][0]
-
-        niped, nzped = get_ni(neped, zeff=zeffped)
-        teped = 0.5*pped/(1.602*neped)
-        tiped = 0.5*pped/(1.602*(niped+nzped))
-
-        print('neped:', neped)
-        print('niped:', niped)
-        print('nzped:', nzped)
-        print('teped:', teped)
-        print('tiped:', tiped)
-
-        ps = plasmastate('ips', 1)
-        ps.read(cur_state_file)
-
-        rho   = ps["rho"][:]
-        nrho  = len(rho)
-        ne = ps["ns"][0,:]*1.0e-19
-        te = ps["Ts"][0,:]
-        ti = ps["Ti"][:]
-        ne = ps.cell2node_bdry(ne)
-        te = ps.cell2node_bdry(te)
-        ti = ps.cell2node_bdry(ti)
-
-        alpha = 1.5
-        beta = 1.5
-        xtop = 1.0-1.5*wped
-
-        ne_model = profile_pedestal(nrho, 1.0-0.5*wped, wped, neped, nesep, ne[0], alpha, beta)
-        te_model = profile_pedestal(nrho, 1.0-0.5*wped, wped, teped, tesep, te[0], alpha, beta)
-        ti_model = profile_pedestal(nrho, 1.0-0.5*wped, wped, tiped, tisep, ti[0], alpha, beta)
-
-        ne_model_top = ne_model(xtop)
-        te_model_top = te_model(xtop)
-        ti_model_top = ti_model(xtop)
-
-        ne_top = zinterp(rho,ne)(xtop)
-        te_top = zinterp(rho,te)(xtop)
-        ti_top = zinterp(rho,ti)(xtop)
-
-        ne_update = zeros(nrho)
-        te_update = zeros(nrho)
-        ti_update = zeros(nrho)
-
-        print("ne top", ne_model_top , ne_top)
-        print("te top", te_model_top , te_top)
-        print("ti top", ti_model_top , ti_top)
-
-        for i in range(nrho):
-            if rho[i] < xtop:
-                ne_update[i] = ne[i] + ne_model_top - ne_top
-                te_update[i] = te[i] + te_model_top - te_top
-                ti_update[i] = ti[i] + ti_model_top - ti_top
-            else:
-                ne_update[i] = ne_model(rho[i])
-                te_update[i] = te_model(rho[i])
-                ti_update[i] = ti_model(rho[i])
-
-        ps["ns"][0] = 1.0e19*ps.node2cell(ne_update)
-        ps["Ts"][0] = ps.node2cell(te_update)
-        nspec_th = len(ps["Ts"])-1
-        print('nspec_th =',nspec_th)
-        for k in range(nspec_th):
-            ps["Ts"][k+1] = ps.node2cell(ti_update)
-        ps["Ti"][:] = ps.node2cell(ti_update)
-
-        ps.store(cur_state_file)
+        pedestal_io.write_input(cur_instate_file, cur_state_file, ps_backend) 
+        pedestal_io.update_state(fn_inped, cur_instate_file, cur_state_file, ps_backend)
 
         #--- update plasma state files
-        services.update_state()
+        self.services.update_state()
 
         #--- archive output files
-        services.stage_output_files(timeStamp, self.OUTPUT_FILES)
+        self.services.stage_output_files(timeid, self.OUTPUT_FILES)
 
-    def finalize(self, timeStamp=0.0):
+    def finalize(self, timeid=0):
         print('pedestal.finalize() called')
+
--- src/fastran/stability/pedestal_io.py
+++ src/fastran/stability/pedestal_io.py
@@ -10,45 +10,212 @@ from fastran.util.modelprofile import profile_pedestal
 from fastran.util.zinterp import zinterp
 from fastran.util.loglinear import loglinear
 from fastran.util.formula import get_ni
+from fastran.util import formula
+from fastran.util.fastranutil import namelist_default
+from fastran.plasmastate.plasmastate import plasmastate
 
-def patch_pedestal(rho, yold, rhob, wped, yped, ysep, alpha=1.1, beta=1.1):
-    nrho = len(rho)
-    ynew = np.zeros(nrho)
-
-    rhotop = 1.0-1.5*wped
-    y1 = profile_pedestal(nrho, 1.0-0.5*wped, wped, yped, ysep, yold[0], alpha, beta)
-    y0 = zinterp(rho/rhob, yold)
-
-    for k in range(nrho):
-        if (rho[k]<=rhotop):
-           ynew[k] = y0( rho[k]/rhotop ) + y1(rhotop) - y0(1.0)
-        else:
-           ynew[k] = y1(rho[k])
-
-    return ynew
-
-def update_instate_pedestal(fn_instate, fn_inpedestal):
+def write_input(fn_instate, fn_plasma_state, ps_backend):
     instate = Namelist(fn_instate)
-    ip = instate["instate"]["ip"][0]
-    neped = instate["instate"]["neped"][0]
-    nesep = instate["instate"]["nesep"][0]
-    zeffped = instate["instate"]["zeff0"][0]
 
-    fits = {}
-    fits["pped"] = loglinear(fn_inpedestal, "pped")
-    fits["wped"] = loglinear(fn_inpedestal, "wped")
+    bt = namelist_default(instate, "instate", "b0", [-1.])[0]
+    ip = namelist_default(instate, "instate", "ip", [-1.])[0]
+    model_shape = namelist_default(instate, "instate", "model_shape", [0])[0]
+    if model_shape in [0]:
+        rb = instate["instate"]["rbdry"] 
+        zb = instate["instate"]["zbdry"] 
+        r = 0.5*( max(rb) + min(rb) )
+        z = 0.5*( max(zb) + min(zb) )
+        a = 0.5*( max(rb) - min(rb) )
+        kappa = 0.5*( ( max(zb) - min(zb) )/ a )
+        delta_u = ( r - rb[argmax(zb)] )/a
+        delta_l = ( z - rb[argmin(zb)] )/a
+        delta = 0.5*( delta_u + delta_l )
+    elif model_shape in [1, 2]:
+        r = namelist_default(instate, "instate", "r0", [-1.])[0] 
+        a = namelist_default(instate, "instate", "a0", [-1.])[0] 
+        kappa = namelist_default(instate, "instate", "kappa", [-1.])[0] 
+        delta = namelist_default(instate, "instate", "delta", [-1.])[0]
+    else:
+        raise Exception("model_shape = {}, not in [0, 1, 2]".format(model_shape))
+    neped = instate["instate"]["ne_ped"][0]
+    nesep = instate["instate"]["ne_sep"][0]
 
-    wped = fits["wped"].get(ip=ip, neped=neped, nesep=nesep)
-    pped = fits["pped"].get(ip=ip, neped=neped, nesep=nesep)
+    xmid = instate["instate"]["xmid"][0]
+    xwid = instate["instate"]["xwid"][0]
+    rho_ped = xmid - 0.5*xwid
 
-    niped, nzped = get_ni(ne=neped, zeff=zeffped)
-    teped= 0.5*pped/(1.602*neped)
-    tiped= 0.5*pped/(1.602*niped)
+    #betan = instate["instate"]["betan"][0]
+    if ps_backend == "INSTATE":
+        rho = np.array(instate["instate"]["rho"])
+        zeff = np.array(instate["instate"]["zeff"])
+        zeff_ped = zinterp(rho, zeff)(rho_ped)
 
-    instate["instate"]["xmid"] = [1.0-0.5*wped]
-    instate["instate"]["xwid"] = [wped]
+        ne = np.array(instate["instate"]["ne"])
+        ni = np.array(instate["instate"]["ni"])
+        nz = np.array(instate["instate"]["nz"])
+        te = np.array(instate["instate"]["te"])
+        ti = np.array(instate["instate"]["ti"])
+        wbeam = np.array(instate["instate"]["wbeam"])
+        vol = np.array(instate["inmetric"]["vol"])
 
-    instate["instate"]["teped"] = [teped]
-    instate["instate"]["tiped"] = [tiped]
+        wth  = 1.5*1.602e3*(ne*te+(ni+nz)*ti)
+        betan_th = formula.betan(w=wth, vol=vol, ip=ip, b0=bt, a0=a)
+        betan_beam = formula.betan(w=wbeam*1.0e6, vol=vol, ip=ip, b0=bt, a0=a)
+        betan = betan_th + betan_beam
+
+    elif ps_backend == "PS":
+        ps = plasmastate('ips', 1)
+        ps.read(fn_plasma_state)
+    
+        rho = ps["rho"][:]
+        zeff = ps.cell2node(ps["Zeff"][:])
+        zeff_ped = zinterp(rho, zeff)(rho_ped)
+
+        ne = ps.cell2node(ps["ns"][0, :]*1.0e-19)
+        ni = ps.cell2node(np.sum(ps["ns"][1:, :], axis=0)*1.0e-19)
+        te = ps.cell2node(ps["Ts"][0, :])
+        ti = ps.cell2node(ps["Ti"][:])
+
+        density_beam = ps.dump_profile(rho, "rho_nbi", "nbeami", k=0)*1.e-19
+        wbeam = ps.dump_profile(rho, "rho_nbi", "eperp_beami", k=0) \
+            + ps.dump_profile(rho, "rho_nbi", "epll_beami", k=0)
+        wbeam = density_beam*wbeam
+    
+        density_alpha = ps.dump_profile(rho, "rho_fus", "nfusi", k=0)*1.e-19
+        walpha = ps.dump_profile(rho, "rho_fus", "eperp_fusi", k=0) \
+            + ps.dump_profile(rho, "rho_fus", "epll_fusi", k=0)
+        walpha = density_alpha*walpha
+    
+        vol = zinterp(ps["rho_eq"], ps["vol"])(rho)
+        wtot = 1.5*1.602e3*(ne*te + ni*ti) + 1.602e3*(wbeam + walpha)
+        betan = formula.betan(w=wtot, vol=vol, ip=ip, b0=bt, a0=a)
+       
+    input_eped = Namelist()
+    input_eped["input_eped"]["r"] = [r]
+    input_eped["input_eped"]["a"] = [a]
+    input_eped["input_eped"]["kappa"] = [kappa]
+    input_eped["input_eped"]["delta"] = [delta]
+    input_eped["input_eped"]["bt"] = [bt]
+    input_eped["input_eped"]["ip"] = [ip] 
+    input_eped["input_eped"]["neped"] = [neped] 
+    input_eped["input_eped"]["nesep"] = [nesep] 
+    input_eped["input_eped"]["zeffped"] = [zeff_ped] 
+    input_eped["input_eped"]["betan"] = [betan]
+    input_eped.write("input_eped")
+
+def update_state(fn_inped, fn_instate, fn_plasma_state, ps_backend="INSTATE"):
+    wped_model = loglinear(fn_inped, 'wped')
+    pped_model = loglinear(fn_inped, 'pped')
+
+    input_eped = Namelist("input_eped")
+
+    wped = wped_model.get_(input_eped["input_eped"])
+    pped = pped_model.get_(input_eped["input_eped"])
+
+    ne_ped = input_eped["input_eped"]["neped"][0]
+    zeff_ped = input_eped["input_eped"]["zeffped"][0]
+
+    ni_ped, nz_ped = get_ni(ne_ped, zeff=zeff_ped)
+    te_ped = 0.5*pped/(1.602*ne_ped)
+    ti_ped = 0.5*pped/(1.602*(ni_ped+nz_ped))
+
+    rho_ped = 1. - wped
+    rho_top = 1. - 1.5*wped
+
+    print('wped = {}'.format(wped))
+    print('pped = {}'.format(pped))
+    print('ne_ped = {}'.format(ne_ped))
+    print('ni_ped = {}'.format(ni_ped))
+    print('nz_ped = {}'.format(nz_ped))
+    print('te_ped = {}'.format(te_ped))
+    print('ti_ped = {}'.format(ti_ped))
+    print('rho_ped = {}'.format(rho_ped))
+    print('rho_top = {}'.format(rho_top))
+
+    instate = Namelist(fn_instate)
+
+    rho_ped_in, rho_top_in = {}, {}
+    for key in ["te", "ti", "ne"]:
+        wped_in = instate["instate"][key+"_xwid"][0]
+        xmid_in = instate["instate"][key+"_xmid"][0]
+        rho_ped_in[key] = 1. - wped_in
+        rho_top_in[key] = 1. - 1.5*wped_in
+
+    print("rho_ped_in = {}".format(rho_ped_in))
+    print("rho_top_in = {}".format(rho_top_in))
+
+    y_in = {}
+    if ps_backend == "INSTATE":
+        rho = instate["instate"]["rho"]
+        nrho = instate["instate"]["nrho"][0]
+        for key in ["te", "ti", "ne"]:
+            y_in[key] = instate["instate"][key]
+    elif ps_backend == "PS":
+        ps = plasmastate('ips', 1)
+        ps.read(fn_plasma_state)
+        rho   = ps["rho"][:]
+        nrho = len(rho)
+        y_in["ne"] = ps.cell2node_bdry(ps["ns"][0,:])*1.0e-19
+        y_in["te"] = ps.cell2node_bdry(ps["Ts"][0,:])
+        y_in["ti"] = ps.cell2node_bdry(ps["Ti"][:])
+
+    prof_in = {}
+    for key in ["te", "ti", "ne"]:
+        prof_in[key] = zinterp(rho, y_in[key]) 
+    
+    ysep_in, ytop_in, yaxis_in = {}, {}, {}
+    for key in ["te", "ti", "ne"]:
+        ysep_in[key] = instate["instate"][key+"_sep"][0]
+        ytop_in[key] = prof_in[key](rho_top_in[key])
+        yaxis_in[key] = y_in[key][0]
+
+    te_pedestal = profile_pedestal(nrho, 1.0-0.5*wped, wped, te_ped, ysep_in["te"], yaxis_in["te"], alpha=1.5, beta=1.5)
+    ti_pedestal = profile_pedestal(nrho, 1.0-0.5*wped, wped, ti_ped, ysep_in["ti"], yaxis_in["ti"], alpha=1.5, beta=1.5)
+    ne_pedestal = profile_pedestal(nrho, 1.0-0.5*wped, wped, ne_ped, ysep_in["ne"], yaxis_in["ne"], alpha=1.5, beta=1.5)
+
+    te_top = te_pedestal(rho_top)
+    ti_top = ti_pedestal(rho_top)
+    ne_top = ne_pedestal(rho_top)
+
+    te = np.zeros(nrho)
+    ti = np.zeros(nrho)
+    ne = np.zeros(nrho)
+    for k in range(nrho):
+        if rho[k] < rho_top:
+            te[k] = prof_in["te"](rho[k] * rho_top_in["te"]/rho_top) + te_top - ytop_in["te"]
+            ti[k] = prof_in["ti"](rho[k] * rho_top_in["ti"]/rho_top) + ti_top - ytop_in["ti"]
+            ne[k] = prof_in["ne"](rho[k] * rho_top_in["ne"]/rho_top) + ne_top - ytop_in["ne"]
+        else:
+            te[k] = te_pedestal(rho[k])
+            ti[k] = ti_pedestal(rho[k])
+            ne[k] = ne_pedestal(rho[k])
+
+    instate["instate"]["te"] = te
+    instate["instate"]["te_ped"] = [te_ped]
+    instate["instate"]["te_xwid"] = [wped] 
+    instate["instate"]["te_xmid"] = [1.0 - wped] 
+
+    instate["instate"]["ti"] = ti
+    instate["instate"]["ti_ped"] = [ti_ped]
+    instate["instate"]["ti_xwid"] = [wped] 
+    instate["instate"]["ti_xmid"] = [1.0 - wped] 
+
+    instate["instate"]["ne"] = ne
+    instate["instate"]["ne_ped"] = [ne_ped]
+    instate["instate"]["ne_xwid"] = [wped] 
+    instate["instate"]["ne_xmid"] = [1.0 - wped] 
+
+    if ps_backend == "INSTATE":
+        instate["instate"]["ne"] = ne
+        instate["instate"]["te"] = te
+        instate["instate"]["ti"] = ti
+    elif ps_backend == "PS":
+        ps["ns"][0] = 1.0e19*ps.node2cell(ne)
+        ps["Ts"][0,:] = ps.node2cell(te)
+        nspec_th = len(ps["Ts"])-1
+        for k in range(nspec_th):
+            ps["Ts"][k+1,:] = ps.node2cell(ti)
+        ps["Ti"][:] = ps.node2cell(ti)
+        ps.store(fn_plasma_state)
 
     instate.write(fn_instate)
