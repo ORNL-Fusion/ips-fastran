c99b3a0b0ee3ac4bfe020876fb9cbfa4d9b76d26
JM <parkjm@fusion.gat.com>
2022-09-19 08:36:05 -0700

    use modeleq_constraint.py for the PS backend

--- src/fastran/driver/modeleq_constraint.py
+++ src/fastran/driver/modeleq_constraint.py
@@ -2,6 +2,8 @@
 model equilibrium, profile adjust
 """
 from fastran.driver.modeleq_constraint_io import update_state, constraint_pedestal_width
+from fastran.plasmastate.plasmastate import plasmastate
+from fastran.instate import instate_io
 from ipsframework import Component
 
 class modeleq_constraint(Component):
@@ -17,7 +19,11 @@ class modeleq_constraint(Component):
 
         services = self.services
         services.stage_state()
+
+        ps_update = getattr(self, 'PS_UPDATE', 'disabled')
         cur_instate_file = services.get_config_param('CURRENT_INSTATE')
+        if ps_update == 'enabled':
+            cur_state_file = services.get_config_param('CURRENT_STATE')
 
         update_state(timestamp,cur_instate_file,nmax_iter=100,const=None)
 
@@ -25,6 +31,12 @@ class modeleq_constraint(Component):
 
         if k_pedestal_constraint >= 0 and timestamp >= k_pedestal_constraint:
             constraint_pedestal_width(cur_instate_file)
+
+        if ps_update == 'enabled':
+            ps = plasmastate('ips', 1)
+            ps.read(cur_state_file)
+            instate_io.instate_to_ps(cur_instate_file, ps)
+            ps.store(cur_state_file)
 
         services.update_state()
         services.stage_output_files(timestamp, self.OUTPUT_FILES)
