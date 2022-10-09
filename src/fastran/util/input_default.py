4fa95301feec19ffad5bf38605a0d23516f0c733
parkjm <parkjm@fusion.gat.com>
2022-10-09 15:29:10 -0700

    add utility routine for component default

---
+++ src/fastran/util/input_default.py
@@ -0,0 +1,23 @@
+from Namelist import Namelist
+
+def input_default(self, key="", default="", alias=[], maps={}, verb=True):
+    val = ""
+    for _key in [key] + alias:
+       if hasattr(self, _key): 
+           val = getattr(self, _key)
+           if verb:
+               print("INPUT:", _key, "found", val, ", mapped value = ", maps.get(val, val))
+           val =  maps.get(val, val) 
+           break
+    if val == "":
+       if verb: 
+           print("INPUT:", key, "not found, use default value:", default)
+       val = default
+    return val
+
+if __name__=="__main__":
+    class test(): pass
+
+    check = test()
+    check.b = "10"
+    print( input_default(check, key="a", default="enabled", alias=["b"], maps={"10":"enabled"}, verb=True ) )
