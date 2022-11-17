ec16c9e094b054bd75547865ab9ffb789ed37b22
parkjm <parkjm@fusion.gat.com>
2022-11-17 11:08:43 -0800

    debug shape_io.py - explicit numpy namespace

--- src/fastran/util/shape_io.py
+++ src/fastran/util/shape_io.py
@@ -1,19 +1,18 @@
+import numpy as np
 # Luce shape
-try:
-    from omfit_classes.fluxSurface import boundaryShape
-except:
-    from omfit.classes.fluxSurface import boundaryShape
+from omfit_classes.fluxSurface import boundaryShape
+#from omfit.classes.fluxSurface import boundaryShape
 
 # simple analytic
 def set_shape(R0, a0, kappa, delta, nt, dlim = 0.05):
-    t = linspace(0.0, 2.*pi, nt)
-    rb = R0 + a0*cos(t+delta*sin(t))
-    zb = kappa*a0*sin(t)
+    t = np.linspace(0.0, 2.*np.pi, nt)
+    rb = R0 + a0*np.cos(t+delta*np.sin(t))
+    zb = kappa*a0*np.sin(t)
 
-    rmax = max(rb) + dlim
-    rmin = min(rb) - dlim
-    zmax = max(zb) + dlim
-    zmin = min(zb) - dlim
+    rmax = np.max(rb) + dlim
+    rmin = np.min(rb) - dlim
+    zmax = np.max(zb) + dlim
+    zmin = np.min(zb) - dlim
     rlim = [ rmax, rmin, rmin, rmax, rmax ]
     zlim = [ zmax, zmax, zmin, zmin, zmax ]
 
