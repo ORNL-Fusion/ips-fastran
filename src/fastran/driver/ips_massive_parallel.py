5e178664e2eab96f161cdc2cf24bb82d297d9125
parkjm <parkjm@fusion.gat.com>
2023-01-13 17:50:18 -0800

    update massive parallel to use MPI codes inside the container

--- src/fastran/driver/ips_massive_parallel.py
+++ src/fastran/driver/ips_massive_parallel.py
@@ -4,6 +4,7 @@
  -----------------------------------------------------------------------
 """
 import os
+import shutil
 import subprocess
 from threading import Thread, Event
 from configobj import ConfigObj
@@ -12,13 +13,23 @@ from ipsframework import Component
 from distributed.diagnostics.plugin import WorkerPlugin
 
 
-def file_daemon(worker_id, evt, source_dir, target_dir):
+def file_daemon(worker_id, evt, source_dir, target_dir, archive_files):
+    files = archive_files if archive_files else "run?????/run?????.???  run?????/simulation_log/*.eventlog"
+    files = "SUMMARY " + files
+    print(">>> archive_files:", files)
 
     cmd = f"""cd {source_dir}
-tar -caf {target_dir}/{worker_id}_archive_$(date +"%Y-%m-%dT%H.%M.%S").tar.gz SUMMARY run?????
+tar -caf {target_dir}/{worker_id}_archive_$(date +"%Y-%m-%dT%H.%M.%S").tar.gz {files}
 # Remove all *.tar.gz except most recent 2
 ls -t {target_dir}/{worker_id}_archive_*.tar.gz | tail +3 | xargs rm -f
 """
+    print(">>> cmd:", cmd)
+
+#        cmd = f"""cd {source_dir}
+#tar -caf {target_dir}/{worker_id}_archive_$(date +"%Y-%m-%dT%H.%M.%S").tar.gz SUMMARY run?????/run?????.???  run?????/simulation_log/*.eventlog
+## Remove all *.tar.gz except most recent 2
+#ls -t {target_dir}/{worker_id}_archive_*.tar.gz | tail +3 | xargs rm -f
+#"""
 
     while not evt.wait(600):  # interval which to archive data
         os.system(cmd)
@@ -27,13 +38,14 @@ ls -t {target_dir}/{worker_id}_archive_*.tar.gz | tail +3 | xargs rm -f
 
 
 class ArchievingPlugin(WorkerPlugin):
-    def __init__(self, tmp_dir, target_dir):
+    def __init__(self, tmp_dir, target_dir, archive_files):
         self.tmp_dir = tmp_dir
         self.target_dir = target_dir
+        self.archive_files = archive_files
 
     def setup(self, worker):
         self.evt = Event()
-        self.thread = Thread(target=file_daemon, args=(worker.id, self.evt, self.tmp_dir, self.target_dir))
+        self.thread = Thread(target=file_daemon, args=(worker.id, self.evt, self.tmp_dir, self.target_dir, self.archive_files))
         self.thread.start()
 
     def teardown(self, worker):
@@ -73,6 +85,12 @@ class ips_massive_parallel(Component):
 
         dask_nodes = int(getattr(self, "DASK_NODES", "1"))
 
+        task_ppn = int(getattr(self, "TASK_PPN", "-1"))
+        task_ppn = task_ppn if task_ppn > 0 else services.get_config_param("PROCS_PER_NODE")
+        print('task_ppn =', task_ppn)
+
+        task_nproc = int(getattr(self, "TASK_NPROC", "1"))
+
         try:
             pwd = services.get_config_param("PWD")
         except:
@@ -101,17 +119,21 @@ class ips_massive_parallel(Component):
                     sim[comp][vname] = val
 
             sim["PWD"] = tmp_xfs if tmp_xfs else pwd
-            sim["INPUT_DIR_SIM"] = pwd + "/input"
+           #sim["INPUT_DIR_SIM"] = pwd + "/input"
+            sim["INPUT_DIR_SIM_TEMP"] = os.path.realpath(os.path.join(pwd , "input"))
+            sim["INPUT_DIR_SIM"] = os.path.join(rundir , "input")
             sim["RUN_ID"] = "run%05d"%k
             sim["SIM_ROOT"] = rundir
             sim["OUT_REDIRECT"] = "True"
             sim["OUT_REDIRECT_FNAME"] = os.path.join(rundir, "run%05d.out"%k)
+            sim["LOG_FILE"] = os.path.join(rundir, "run%05d.log"%k)
             sim["USE_PORTAL"] = "True"
             try:
                 sim['PARENT_PORTAL_RUNID'] = self.services.get_config_param("PORTAL_RUNID")
                 sim["USE_PORTAL"] = "True"
             except Exception:
                 sim["USE_PORTAL"] = "False"
+            sim["TASK_NPROC"] = task_nproc
             driver = sim['PORTS']['DRIVER']['IMPLEMENTATION']
             sim[driver]["SUMMARY"] = tmp_xfs_dir_summary if tmp_xfs else dir_summary
 
@@ -126,11 +148,13 @@ class ips_massive_parallel(Component):
                 self.TMPXFS,
                 logfile=logfile)
 
-        worker_plugin = ArchievingPlugin(self.TMPXFS, dir_summary)
+        archive_files = getattr(self, "ARCHIVE_FILES", "")
+        worker_plugin = ArchievingPlugin(self.TMPXFS, dir_summary, archive_files)
 
         #--- run
         ret_val = services.submit_tasks('pool', use_dask=True, dask_nodes=dask_nodes,
                                         use_shifter=True,
+                                        dask_ppw=task_ppn,
                                         dask_worker_plugin=worker_plugin)
 
         print('ret_val = ', ret_val)
@@ -152,12 +176,21 @@ def taskRunner(runname, sim, tmp_xfs, timeout=1e9):
     rundir = os.path.join(tmp_xfs, runname)
 
     os.makedirs(rundir, exist_ok=True)
-    wrt_localconf(os.path.join(rundir, "local.conf"))
 
     sim = ConfigObj(eval(sim))
+
+    task_nproc = int(sim["TASK_NPROC"])
+
+    input_dir = sim["INPUT_DIR_SIM_TEMP"] 
+    print(input_dir )
+    shutil.copytree(input_dir, os.path.join(rundir, "input"))
+    #sim["INPUT_DIR_SIM"] =  os.path.join(rundir, "input")
+
     sim.write(open(os.path.join(rundir, f"{runname}.config"), "wb"))
-    
-    ips_bin = "which ips.py\n"
+     
+#   ips_bin = "which ips.py\n"
+    ips_bin = "echo $PYTHONPATH\n"
+    ips_bin += "which ips.py\n"
     ips_bin += "ips.py"
     ips_bin += f" --config={runname}.config"
     ips_bin += f" --log=ips_{runname}.log"
@@ -166,6 +199,8 @@ def taskRunner(runname, sim, tmp_xfs, timeout=1e9):
     ips_bin_path = os.path.join(rundir, "ips_bin.sh")
     with open(ips_bin_path, "w") as f:
         f.write(ips_bin)
+
+    wrt_localconf(os.path.join(rundir, "local.conf"), task_nproc)
 
     process = subprocess.Popen(["bash", "ips_bin.sh"],
                                cwd=rundir)
@@ -176,19 +211,22 @@ def taskRunner(runname, sim, tmp_xfs, timeout=1e9):
         process.kill()
         return -1
 
-def wrt_localconf(fname="local.conf"):
+def wrt_localconf(fname="local.conf", task_nproc=1):
+    if task_nproc == 1:
+       cmd = "eval"
+    else:
+       cmd = "mpiexec"
     s = \
 """HOST = local
-MPIRUN = eval
+MPIRUN = %s  
 NODE_DETECTION = manual
-TOTAL_PROCS = 1
-NODES = 1
-PROCS_PER_NODE = 1
-CORES_PER_NODE = 1
+PROCS_PER_NODE = %d 
+CORES_PER_NODE = %d 
 SOCKETS_PER_NODE = 1
 NODE_ALLOCATION_MODE = SHARED
 USE_ACCURATE_NODES = ON
-"""
+"""%(cmd, task_nproc, task_nproc)
+
     f=open(fname, "w")
     f.write(s)
     f.close()
