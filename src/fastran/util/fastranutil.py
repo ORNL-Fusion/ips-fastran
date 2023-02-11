93e7b456dee079b4aff743801f62dc3075bd32d4
parkjm <parkjm@fusion.gat.com>
2023-02-10 19:34:04 -0800

    add config default

--- src/fastran/util/fastranutil.py
+++ src/fastran/util/fastranutil.py
@@ -6,6 +6,16 @@ def namelist_default(nml, key1, key2, val):
     else:
         return val
 
+def config_default(sim_conf, key1, key2, val):
+     if key1 in sim_conf:
+         if key2 in sim_conf[key1]:
+             return_val = sim_conf[key1][key2]
+         else:
+             return_val = val
+     else:
+         return_val = val
+     return return_val
+
 if __name__=="__main__":
     test = Namelist()
     test["instate"]["ip"] = [1.0]
