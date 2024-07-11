510f005dfd71bacdd3693c5a8da29e68f2e91180
parkjm <parkjm@fusion.gat.com>
2024-07-10 23:02:57 -0700

    remove inbc, feedback + clean up

--- src/fastran/heating/nfreya.py
+++ src/fastran/heating/nfreya.py
@@ -10,6 +10,7 @@ from Namelist import Namelist
 from ipsframework import Component
 from fastran.heating import nfreya_io
 from fastran.util.fastranutil import freeze
+from fastran.util.fastranutil import namelist_default
 
 
 class nfreya(Component):
@@ -24,7 +25,8 @@ class nfreya(Component):
         print('nfreya.step() started')
 
         # -- freeze/resume
-        if freeze(self, timeid, 'nfreya'): return None
+        if freeze(self, timeid, 'nfreya'):
+            return None
 
         # -- excutable
         nfreya_bin = os.path.join(self.BIN_PATH, self.BIN)
@@ -37,11 +39,8 @@ class nfreya(Component):
         ps_backend = getattr(self, 'PS_BACKEND', 'PS')
         if ps_backend == 'PS':
             cur_state_file = self.services.get_config_param('CURRENT_STATE')
-            cur_bc_file = self.services.get_config_param('CURRENT_BC')
         elif ps_backend == 'INSTATE':
             cur_instate_file = self.services.get_config_param('CURRENT_INSTATE')
-            cur_bc_file = cur_instate_file
-
         cur_eqdsk_file = self.services.get_config_param('CURRENT_EQDSK')
 
         # -- stage input files
@@ -53,50 +52,46 @@ class nfreya(Component):
         for key in var_list:
             for k in range(len(innfreya['innfreya'][key])):
                 try:
-                    innfreya['innfreya'][key][k] = float(getattr(self, key+'_%d' % k))
+                    innfreya['innfreya'][key][k] = float(
+                        getattr(self, key + '_%d' % k))
                     print(key, k, 'updated')
                 except AttributeError:
                     pass
-        innfreya.write('innfreya')
-
-        FEEDBACK = int(getattr(self, 'FEEDBACK', '10000'))
-        inbc = Namelist(cur_bc_file)
-        innfreya = Namelist('innfreya', case='lower')
-        _timeid = int(str(timeid).split('_')[-1])
-        if _timeid >= FEEDBACK:
-            pnb = inbc['feedback']['pnb'][-1]
-            innfreya['innfreya']['bptor'] = [pnb]
-            print('feedback:')
-            print(inbc['feedback']['pnb'])
-            print(pnb)
-        else:
-            inbc['feedback']['pnb'] = [innfreya['innfreya']['bptor'][0]]
-        inbc.write(cur_bc_file)
         innfreya.write('innfreya')
 
         # -- generate input
         f_innfreya = 'innfreya'
         try:
             dir_data = self.DIR_DATA
-        except:
+        except BaseException:
             dir_data = ''
         print(dir_data)
 
         if ps_backend == 'PS':
-            nfreya_io.write_inputfiles(cur_state_file, cur_eqdsk_file, f_innfreya, dir_data)
+            nfreya_io.write_inputfiles(
+                cur_state_file, cur_eqdsk_file, f_innfreya, dir_data)
         elif ps_backend == 'INSTATE':
-            nfreya_io.write_inputfiles_instate(cur_instate_file, cur_eqdsk_file, f_innfreya, dir_data)
+            nfreya_io.write_inputfiles_instate(
+                cur_instate_file, cur_eqdsk_file, f_innfreya, dir_data)
 
         # -- run nfreya
         print('run nfreya')
         if int(getattr(self, 'SERIAL', '0')) == 1:
             print('nfreya, subprocess')
             logfile = open('xnfreya.log', 'w')
-            retcode = subprocess.call([nfreya_bin], stdout=logfile, stderr=logfile, shell=True)
+            retcode = subprocess.call(
+                [nfreya_bin],
+                stdout=logfile,
+                stderr=logfile,
+                shell=True)
             logfile.close()
         else:
             cwd = self.services.get_working_dir()
-            task_id = self.services.launch_task(1, cwd, nfreya_bin, logfile='xnfreya.log')
+            task_id = self.services.launch_task(
+                1, 
+                cwd, 
+                nfreya_bin, 
+                logfile='xnfreya.log')
             retcode = self.services.wait_task(task_id)
 
         if (retcode != 0):
@@ -119,12 +114,20 @@ class nfreya(Component):
             self.services.update_state()
 
         # -- archive output files
-        self.services.stage_output_files(timeid, self.OUTPUT_FILES, save_plasma_state=False)
+        self.services.stage_output_files(
+            timeid, self.OUTPUT_FILES, save_plasma_state=False)
 
         # -- clean up
         clean_after = int(getattr(self, 'CLEAN_AFTER', '1'))
         if clean_after:
-            delete_files = ['bpltfil', 'eqpltfil', 'namelists', 'qikone', 'isllog', 'runlog', 'test_trnspt_mhd.txt']
+            delete_files = [
+                'bpltfil',
+                'eqpltfil',
+                'namelists',
+                'qikone',
+                'isllog',
+                'runlog',
+                'test_trnspt_mhd.txt']
             for f in delete_files:
                 if os.path.exists(f):
                     os.remove(f)
