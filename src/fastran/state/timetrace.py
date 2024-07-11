bf22ad6ee41e35bdc515a2bb0a12b48813d48ba4
parkjm <parkjm@fusion.gat.com>
2024-07-10 22:59:04 -0700

    add EC power to timetrace + clean up

--- src/fastran/state/timetrace.py
+++ src/fastran/state/timetrace.py
@@ -13,7 +13,7 @@ class Timetrace():
 
         # equilibrium
         for key in ['time_eq', 'rho_eq']:
-            self.data[key] =  nc.variables[key][:]
+            self.data[key] = nc.variables[key][:]
 
         for key in ['p_eq', 'j_tot']:
             t = self.data['time_eq']
@@ -25,8 +25,9 @@ class Timetrace():
         for key in ['ip', 'bt', 'r0']:
             t = self.data['time_eq']
             f = nc.variables[key][:]
-            self.data[key] =  f
-            self.interpolation[key] = interp1d(t, f, kind=kind, fill_value=(f[0], f[-1],), bounds_error=False)
+            self.data[key] = f
+            self.interpolation[key] = interp1d(
+                t, f, kind=kind, fill_value=(f[0], f[-1],), bounds_error=False)
 
         # profile
         var_profile = ['ne', 'te', 'ti', 'omega', 'nz']
@@ -40,20 +41,21 @@ class Timetrace():
                 self.data[f'time_{key}'] = t
                 self.data[f'rho_{key}'] = x
                 self.data[key] = f
-                self.interpolation[key] = interp2d(t, x, f.transpose(), kind=kind)
+                self.interpolation[key] = interp2d(
+                    t, x, f.transpose(), kind=kind)
 
         # limiter
-        self.data['rlim'] =  nc.variables['rlim'][:]
-        self.data['zlim'] =  nc.variables['zlim'][:]
-        self.data['nlim'] =  len(self.data['rlim'])
+        self.data['rlim'] = nc.variables['rlim'][:]
+        self.data['zlim'] = nc.variables['zlim'][:]
+        self.data['nlim'] = len(self.data['rlim'])
 
         # plasma shape
-        self.data['nbdry'] =  nc.variables['nbdry'][:]
-        self.data['rbdry'] =  nc.variables['rbdry'][:, :]
-        self.data['zbdry'] =  nc.variables['zbdry'][:, :]
+        self.data['nbdry'] = nc.variables['nbdry'][:]
+        self.data['rbdry'] = nc.variables['rbdry'][:, :]
+        self.data['zbdry'] = nc.variables['zbdry'][:, :]
 
         # nbi power
-        var_nbi = ['pnbi'] #, ['pnbi', 'einj', 'ffull', 'fhalf']
+        var_nbi = ['pnbi']  # ['pnbi', 'einj', 'ffull', 'fhalf']
         for key in var_nbi:
             if key in list(nc.variables.keys()):
                 self.data[key] = nc.variables[key][:, :]
@@ -61,7 +63,20 @@ class Timetrace():
                 for k in range(len(self.data[key])):
                     t = nc.variables['time_nbi'][:]
                     f = nc.variables[key][k, :]
-                    self.interpolation[key][k] = interp1d(t, f, kind=kind, fill_value=(f[0], f[-1],), bounds_error=False) 
+                    self.interpolation[key][k] = interp1d(
+                        t, f, kind=kind, fill_value=(f[0], f[-1],), bounds_error=False)
+
+        # ec power
+        var_ech = ['pech']  # ['pech', 'aziang', 'polang']
+        for key in var_ech:
+            if key in list(nc.variables.keys()):
+                self.data[key] = nc.variables[key][:, :]
+                self.interpolation[key] = len(self.data[key]) * [None]
+                for k in range(len(self.data[key])):
+                    t = nc.variables['time_ech'][:]
+                    f = nc.variables[key][k, :]
+                    self.interpolation[key][k] = interp1d(
+                        t, f, kind=kind, fill_value=(f[0], f[-1],), bounds_error=False)
 
         nc.close()
 
@@ -85,25 +100,26 @@ class Timetrace():
         return self.interpolation[key](t, x).flatten()
 
     def slice_list(self, key, t):
-        return [self.interpolation[key][k](t) for k in range(len(self.interpolation[key]))]
+        return [self.interpolation[key][k](t) 
+            for k in range(len(self.interpolation[key]))]
 
     def nearest(self, key, t):
         delta = np.abs(self.times - t)
         i = np.argmin(delta)
         return self.data[key][i]
-        
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # test
-    ncfile = sys.argv[1] # convention: t<shot_number>.nc
+    ncfile = sys.argv[1]  # convention: t<shot_number>.nc
     timetrace = Timetrace(ncfile)
     ne = timetrace['ne']
 
     t = 0.5 * (timetrace.tmin + timetrace.tmax)
     rho = np.linspace(0., 1., 20)
-    print(timetrace.slice('ne', t, rho) )
+    print(timetrace.slice('ne', t, rho))
 
     t = np.linspace(timetrace.tmin, timetrace.tmax, 100)
     rho = 0.5
-    print(timetrace.slice('ne', t, rho) )
+    print(timetrace.slice('ne', t, rho))
     print(timetrace.get('ip', t))
