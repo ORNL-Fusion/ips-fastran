9c395d67ef076af03924b39dde54744d8bc05122
Yashika Ghai <yashu_gh92@gmail.com>
2026-03-28 13:09:39 -0700

    Creating a clean KSTAR branch

--- lib/namelist/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='Namelist',
-    version='0.1',
-    py_modules=['Namelist']
-)
--- lib/omfit/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-package='omfit_classes'
-setup(
-   name=package,
-   version='0.0.0',
-   packages=[package],
-)
